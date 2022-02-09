efbb3c230cb91a3b1fc4ad74c8a5b43376dd5736
Jim Rogers <thejimrogers@pm.me>
2022-02-09 15:28:57 -0600

    Add classes, methods

--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -10,7 +10,15 @@ namespace ExtensionMethods
     {
         static void Main(string[] args)
         {
+            PersonModel person = new PersonModel();
 
+            person.FirstName = "What is your first name: ".RequestString();
+
+            person.LastName = "What is your last name: ".RequestString();
+
+            person.Age = "What is your age: ".RequestInt(0, 120);
+
+            Console.Write("Press ENTER to exit.");
             Console.ReadLine();
         }
     }
